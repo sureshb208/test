8730a523d56f66d5f1daa30cba9eccb25bf4fa37
suresh babu <sureshb208@gmail.com>
2017-10-19 11:33:22 +0530

    added gen/set.

--- example/src/example/student.java
+++ example/src/example/student.java
@@ -5,6 +5,26 @@ public class student {
 	private int id;
 	private String name;
 	private String fname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
 }
+
 
 
